72e71cfcabf383a194d37ebc06190a012e0701aa
Alan Burr <31518721+Dra9onHunter72@users.noreply.github.com>
2017-10-06 12:51:33 -0600

    Added an IBOutlet reference for the buttton

--- FirstAfternoonDemo/ViewController.swift
+++ FirstAfternoonDemo/ViewController.swift
@@ -8,14 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var FirstButton: UIButton!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
